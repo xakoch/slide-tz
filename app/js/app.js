8ca498dae9db040483a7fe4facd2aa37e98c74da
Timur Janov <xakoch13@gmail.com>
2022-11-08 16:07:31 +0500

    adaptive & button animations

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,10 @@
+// vars video
 const video = document.getElementById('video')
 const video_height = document.getElementById('video').offsetHeight
-const btnText = document.getElementById('btn_text')
-const btnIcon = document.getElementById('btn_icon')
-
 let scrl_start = (window.innerHeight - video_height) / 2
 let scrl_end = (window.innerHeight - video_height) / 2 + video_height
 
+// on scroll video animation
 function scrollVideo() {
 
     let section_scroll = window.scrollY % window.innerHeight
@@ -14,19 +13,29 @@ function scrollVideo() {
         if (section_scroll >= scrl_start && section_scroll <= scrl_end) {
             // video[i].style.position = "fixed"
             // var videoPerc= video.innerHTML = `${((section_scroll - scrl_start) / video_height * 100).toFixed(0)}%`
-            gsap.from('.section__cta--icon', { x: 50, autoAlpha: 0, ease: Power1.out })
         } else {
             // video.innerHTML = `no scroll`
             // video[i].style.position = "absolute"
         }
     }
 
-    console.log('section_scroll = ' + section_scroll)
-    console.log('start = ' + scrl_start)
-    console.log('end = ' + scrl_end)
-    // console.log('percent = ' + videoPerc)
 }
 
 scrollVideo()
-
 window.addEventListener("scroll", scrollVideo)
+
+
+
+// on scroll animation buttons
+window.addEventListener('scroll', function() {
+    let scrollY = window.scrollY
+
+    const sections = document.querySelectorAll('.section')
+    for (var i = 0; i < sections.length; i++) {
+        
+        if ( scrollY > sections[i].offsetTop && scrollY < sections[i].offsetTop + sections[i].offsetHeight ) {
+            sections[i].classList.add("is-active");
+        }
+    }
+
+})
